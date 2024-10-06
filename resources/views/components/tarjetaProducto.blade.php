ee4afb5182dc97a4fe66f68aa25e4c120e89c4d8
MikaelaGisbert <karlamikaela.911@gmail.com>
2024-10-05 22:59:36 -0400

    catalogo

---
+++ resources/views/components/tarjetaProducto.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('css/tarjeta_Producto.css') }}">
+    <title>Document</title>
+</head>
+<body>
+    <div class="contenedor_tarjeta">
+        <div class="contenedor_imagen_tarjeta">
+            <img src="{{ asset('images/labial.png') }}" alt="Producto" class="producto_catalogo">
+        </div>
+        <h3 class="informacion_producto">Labial ESIKA</h3>
+        <h3 class="informacion_producto">Bs. 35</h3>
+    </div>
+    
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Route::get('/', function () {
+//     return view('producto');
+// });
+
 Route::get('/', function () {
-    return view('producto');
+    return view('catalogo');
 });
